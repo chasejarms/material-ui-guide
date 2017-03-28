ebbfc0a92aa22138374828c1565418a1e7ef25e9
Chase Armstrong <chasejarms@gmail.com>
2017-03-27 21:57:10 -0700

    Finish initial installation

---
+++ frontend/components/app.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const App = () => (
+  <div>Now inside of the App Component</div>
+);
+
+export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
 const path = require('path');
 
 module.exports = {
-  entry: "./material_you_and_i.js",
+  entry: "./frontend/material_you_and_i.js",
   output: {
-    path: path.resolve(__dirname, "frontend"),
+    path: path.resolve(__dirname),
     filename: "bundle.js",
   },
   module: {
